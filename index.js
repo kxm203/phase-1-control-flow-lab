191504a1127eb78c3020327ec5e1049a1446fc1f
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-01-11 20:10:50 -0500

    Thank you Parker for helping me out!

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
   // Write your code here!
+
+if (depth <= 400){
+  return "This one is on me!";
+} 
+else if (depth >=400 && depth <=2000){
+  return "That will be twenty bucks.";
+} 
+else if (depth > 2000 && depth <= 2500){
+  return "I will gladly take your thirty bucks.";
+} 
+else if (depth > 2500){
+  return "No can do.";
+}
 }
 
-function ternaryCheckCity(){
+
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city=="NYC") ?('Ok, sounds good.'):('No go.');
+  
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+switch (tip) {
+  case "generous":
+    return "Thank you so much.";
+  case "not as generous":
+    return "Thank you.";
+  case "thanks for everything":
+    return "Bye.";
+   
+  
+
+}
+
 }
